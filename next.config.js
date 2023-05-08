1692901c4e08b2b40054bef18a69e08429469910
Shafinaz Shafique <shafinaz.shafique@zemind.ca>
2023-05-08 12:24:09 -0700

    change next-config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const withImages = require("next-images");
 
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = withImages();
+module.exports = nextConfig
+module.exports = withImages({
+  webpack(config, options) {
+    return config
+  }
+})
